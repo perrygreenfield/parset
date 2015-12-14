32683ba0ffc52e154c85d29badc7f3e26e7208d3
perrygreenfield <perry@stsci.edu>
2015-12-14 15:08:18 -0500

    undid test change

--- parset/__init__.py
+++ parset/__init__.py
@@ -11,7 +11,6 @@ from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
-#if not _ASTROPY_SETUP_:
-if True:
+if not _ASTROPY_SETUP_:
 	from .core import *
 	from .ttypes import *
